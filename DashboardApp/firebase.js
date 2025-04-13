5cfb35701d659145988e1fb4a76f62bd88af1740
Neha Lamboria <105237382+NehaLamboria@users.noreply.github.com>
2025-04-13 22:08:01 +0530

    Create firebase.js

---
+++ DashboardApp/firebase.js
@@ -0,0 +1,13 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBoQlKcvdbmDfx9qOdD0ANWCXo8H3O7ifg",
+    authDomain: "dashboard-d1af7.firebaseapp.com",
+    databaseURL: "https://dashboard-d1af7-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "dashboard-d1af7",
+    storageBucket: "dashboard-d1af7.firebasestorage.app",
+    messagingSenderId: "639084423552",
+    appId: "1:639084423552:web:327e2c4a054402b142f965"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
