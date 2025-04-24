197c06fce57620867a7b0e68201dae8b82536103
Neha Lamboria <105237382+NehaLamboria@users.noreply.github.com>
2025-04-24 23:42:51 +0530

    Create App.js

---
+++ L1 - Building components & Layout/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PricingSection from './components/PricingSection';
+import './App.css'; // You might have global styles here
+
+function App() {
+  return (
+    <div className="App">
+      <PricingSection />
+    </div>
+  );
+}
+
+export default App;
