84107609e9a1a877722333c8d3ec45ffdc8c3228
Neha Lamboria <105237382+NehaLamboria@users.noreply.github.com>
2025-04-24 23:08:44 +0530

    Create App.js

---
+++ L0 - Implementing Conditional Styling/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ColorToggleButton from "./ColorToggleButton";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <ColorToggleButton />
+    </div>
+  );
+}
+
+export default App;
