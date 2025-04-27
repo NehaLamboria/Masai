82edcb07325863b7f6d57a7fc645001b814e5928
Neha Lamboria <105237382+NehaLamboria@users.noreply.github.com>
2025-04-27 22:55:59 +0530

    Create App.js

---
+++ L1 - Debugging a Multi-Feature To-Do List/to-do list/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToDoList from './components/ToDoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My To-Do List</h1>
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
