7cf437534b3eaa856d780d616543d7b126023e03
Neha Lamboria <105237382+NehaLamboria@users.noreply.github.com>
2025-04-27 22:13:27 +0530

    Create App.js

---
+++ L0 - Toggle Button with Dynamic Message/toggle-button/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToggleMessage from './ToggleMessage'; // import the ToggleMessage component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Toggle Message Example</h1>
+      <ToggleMessage />
+    </div>
+  );
+}
+
+export default App;
